be8d4e18d1f1ca171191283e8e2e76d9786ca775
zero9178 <markus.boeck02@gmail.com>
2020-07-09 16:29:40 +0200

    Initial GCC 8 Release

--- lib/gcc/i686-w64-mingw32/9/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "../configure --target=i686-w64-mingw32 --host=x86_64-w64-mingw32 --disable-bootstrap --enable-libstdcxx-debug --with-tune=znver1 --prefix=/mnt/c/GCC --with-sysroot=/mnt/c/GCC --disable-libstdcxx-pch --disable-multilib --enable-libgomp --with-cross-host --with-libiconv-prefix=/mnt/c/GCC-Build/gcc/build/../../libiconv-1.16/install --disable-libstdcxx-verbose --enable-languages=c,c++,fortran,lto,objc,obj-c++ --disable-nls --disable-win32-registry --enable-shared --with-gnu-as --with-gnu-ld --enable-threads=posix --program-suffix=-9 --enable-version-specific-runtime-libs --with-gcc-major-version-only --enable-__cxa_atexit --program-prefix=i686-w64-mingw32- --enable-plugin";
-static const char thread_model[] = "posix";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "cpu", "generic" }, { "arch_32", "pentiumpro" }, { "arch_64", "x86-64" }, { "tune", "znver1" } };
