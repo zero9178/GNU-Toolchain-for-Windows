44197cdc83db97cb8f377c81392c99c11cef4326
zero9178 <markus.boeck02@gmail.com>
2020-08-05 01:58:10 +0200

    binutils now supports all targets, updated rest

--- lib/gcc/i686-w64-mingw32/8/include/c++/i686-w64-mingw32/bits/c++config.h
+++ lib/gcc/i686-w64-mingw32/8/include/c++/i686-w64-mingw32/bits/c++config.h
@@ -34,7 +34,7 @@
 #define _GLIBCXX_RELEASE 8
 
 // The datestamp of the C++ library in compressed ISO date format.
-#define __GLIBCXX__ 20200729
+#define __GLIBCXX__ 20200804
 
 // Macros for various attributes.
 //   _GLIBCXX_PURE
