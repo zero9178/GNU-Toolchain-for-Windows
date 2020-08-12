740c5ab3b2b17f402dfeee12a46d56abd57424c0
zero9178 <markus.boeck02@gmail.com>
2020-08-12 20:17:53 +0200

    Weekly upadte

--- lib/gcc/x86_64-w64-mingw32/10/plugin/include/plugin-version.h
+++ lib/gcc/x86_64-w64-mingw32/10/plugin/include/plugin-version.h
@@ -6,7 +6,7 @@
 #define GCCPLUGIN_VERSION  (GCCPLUGIN_VERSION_MAJOR*1000 + GCCPLUGIN_VERSION_MINOR)
 
 static char basever[] = "10.2.1";
-static char datestamp[] = "20200804";
+static char datestamp[] = "20200812";
 static char devphase[] = "";
 static char revision[] = "";
 
