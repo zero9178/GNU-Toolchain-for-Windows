5163dc0dbddc8b8cc268277623ee9b1ed0e1a5f6
zero9178 <markus.boeck02@gmail.com>
2020-09-06 03:29:48 +0200

    Weekly update, but reverted binutils and gdb to a version without a segfault when creating dlls with import libraries

--- lib/gcc/i686-w64-mingw32/10/include/c++/i686-w64-mingw32/bits/c++config.h
+++ lib/gcc/i686-w64-mingw32/10/include/c++/i686-w64-mingw32/bits/c++config.h
@@ -34,7 +34,7 @@
 #define _GLIBCXX_RELEASE 10
 
 // The datestamp of the C++ library in compressed ISO date format.
-#define __GLIBCXX__ 20200828
+#define __GLIBCXX__ 20200906
 
 // Macros for various attributes.
 //   _GLIBCXX_PURE
