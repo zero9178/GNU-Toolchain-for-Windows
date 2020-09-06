6c1bf7ca07e577435019d1f2049e7f93d992bffb
zero9178 <markus.boeck02@gmail.com>
2020-09-06 03:18:08 +0200

    Weekly update, but reverted binutils and gdb to a version without a segfault when creating dlls with import libraries

--- lib/gcc/x86_64-w64-mingw32/10/plugin/include/plugin-version.h
+++ lib/gcc/x86_64-w64-mingw32/10/plugin/include/plugin-version.h
@@ -6,7 +6,7 @@
 #define GCCPLUGIN_VERSION  (GCCPLUGIN_VERSION_MAJOR*1000 + GCCPLUGIN_VERSION_MINOR)
 
 static char basever[] = "10.2.1";
-static char datestamp[] = "20200820";
+static char datestamp[] = "20200905";
 static char devphase[] = "";
 static char revision[] = "";
 
