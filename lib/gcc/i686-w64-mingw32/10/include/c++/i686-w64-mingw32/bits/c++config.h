e3d1414a982d808f3f1843eaba203f9765f12bc7
zero9178 <markus.boeck02@gmail.com>
2020-10-20 15:09:38 +0200

    Weekly update

--- lib/gcc/i686-w64-mingw32/10/include/c++/i686-w64-mingw32/bits/c++config.h
+++ lib/gcc/i686-w64-mingw32/10/include/c++/i686-w64-mingw32/bits/c++config.h
@@ -34,7 +34,7 @@
 #define _GLIBCXX_RELEASE 10
 
 // The datestamp of the C++ library in compressed ISO date format.
-#define __GLIBCXX__ 20201012
+#define __GLIBCXX__ 20201020
 
 // Macros for various attributes.
 //   _GLIBCXX_PURE
