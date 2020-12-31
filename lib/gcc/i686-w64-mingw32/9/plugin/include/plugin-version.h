00978ff98d4222c1aa1f73317a0b4b734d55af23
zero9178 <markus.boeck02@gmail.com>
2020-12-31 12:03:59 +0100

    Correctly distribute binutils with a dynamic libdep

--- lib/gcc/i686-w64-mingw32/9/plugin/include/plugin-version.h
+++ lib/gcc/i686-w64-mingw32/9/plugin/include/plugin-version.h
@@ -6,7 +6,7 @@
 #define GCCPLUGIN_VERSION  (GCCPLUGIN_VERSION_MAJOR*1000 + GCCPLUGIN_VERSION_MINOR)
 
 static char basever[] = "9.3.1";
-static char datestamp[] = "20201221";
+static char datestamp[] = "20201230";
 static char devphase[] = "";
 static char revision[] = "";
 
