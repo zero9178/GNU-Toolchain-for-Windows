fac7ce4ab71a13c283a331baf95ccb09bebf5bb9
zero9178 <markus.boeck02@gmail.com>
2021-05-01 22:05:15 +0200

    GCC 11 release

--- lib/gcc/x86_64-w64-mingw32/9/plugin/include/plugin-version.h
+++ lib/gcc/x86_64-w64-mingw32/9/plugin/include/plugin-version.h
@@ -6,7 +6,7 @@
 #define GCCPLUGIN_VERSION  (GCCPLUGIN_VERSION_MAJOR*1000 + GCCPLUGIN_VERSION_MINOR)
 
 static char basever[] = "9.3.1";
-static char datestamp[] = "20210315";
+static char datestamp[] = "20210501";
 static char devphase[] = "";
 static char revision[] = "";
 
