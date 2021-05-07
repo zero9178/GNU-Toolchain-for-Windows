3e269398a29647f6fa670c315c96bc17c0e4fa57
zero9178 <markus.boeck02@gmail.com>
2021-05-07 21:43:57 +0200

    Weekly update

--- lib/gcc/i686-w64-mingw32/11/plugin/include/plugin-version.h
+++ lib/gcc/i686-w64-mingw32/11/plugin/include/plugin-version.h
@@ -6,7 +6,7 @@
 #define GCCPLUGIN_VERSION  (GCCPLUGIN_VERSION_MAJOR*1000 + GCCPLUGIN_VERSION_MINOR)
 
 static char basever[] = "11.1.1";
-static char datestamp[] = "20210501";
+static char datestamp[] = "20210507";
 static char devphase[] = "";
 static char revision[] = "";
 
