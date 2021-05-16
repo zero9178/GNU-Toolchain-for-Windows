59fa57d4a226783cc2a9fe54ead0b3ca4389770d
zero9178 <markus.boeck02@gmail.com>
2021-05-16 12:54:57 +0200

    GCC 8.5 Release and EOL

--- lib/gcc/x86_64-w64-mingw32/9/include/c++/x86_64-w64-mingw32/bits/c++config.h
+++ lib/gcc/x86_64-w64-mingw32/9/include/c++/x86_64-w64-mingw32/bits/c++config.h
@@ -34,7 +34,7 @@
 #define _GLIBCXX_RELEASE 9
 
 // The datestamp of the C++ library in compressed ISO date format.
-#define __GLIBCXX__ 20210507
+#define __GLIBCXX__ 20210516
 
 // Macros for various attributes.
 //   _GLIBCXX_PURE
